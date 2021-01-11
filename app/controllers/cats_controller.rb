6e8d32ce069772b79c23ef43b144794c69c5c0ec
Wesley <wes.beck21@outlook.com>
2021-01-11 07:58:57 -0800

    condition fix

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -36,19 +36,19 @@ class CatsController < ApplicationController
     #create
     #persist new cat to database, and redirect to individual show page with validations
     post "/cats" do
-        if !params["age"].is_a? Integer
+        @cat = current_user.cats.build(params)
+        if !@cat.age.is_a? Integer
             @error = "Cat's age must be a whole number. Please try again."
             erb :"cats/new"
         else
-            @cat = current_user.cats.build(params)
-                if @cat.save
-                    #take user to all cats show page to see their post as the most recent at top
-                    redirect  "/cats"
-                else 
-                    @error = "Cat must have at least a name and age. Please try again."
-                    erb :"cats/new"
-                    #rerender the form
-                end
+            if @cat.save
+                #take user to all cats show page to see their post as the most recent at top
+                redirect  "/cats"
+            else 
+                 @error = "Cat must have at least a name and age. Please try again."
+                erb :"cats/new"
+                #rerender the form
+            end
         end
     end
         
